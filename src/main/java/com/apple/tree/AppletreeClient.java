c6a69215b4ee297d8f5b5d8a5410df48a38d3f9c
avrilzion <maxzinuo@foxmail.com>
2024-09-18 16:00:05 +0800

    first commit

---
+++ src/main/java/com/apple/tree/AppletreeClient.java
@@ -0,0 +1,10 @@
+package com.apple.tree;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AppletreeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/apple/tree/AppletreeDataGenerator.java
@@ -0,0 +1,11 @@
+package com.apple.tree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AppletreeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
